1f3c5acc0d421d94d4b099fccf9b9ab485009bcf
derjacklive <jackruder023@outlook.de>
2023-05-12 22:41:54 +0200

    Fertiggestellte Consolen Version 12.05.23..22:41

---
+++ madlibs-jack-console-vers.py
@@ -0,0 +1,24 @@
+autor = "DJL - Jack" # Creator
+
+print(f"Hinterlasst ein Star für {autor} :D") # Startscreen
+
+lk = ("_____")
+
+print(f"Programmieren ist so {lk}! Es erfreut Mich die ganze zeit, \
+weil ich es zu {lk} finde! <3  Bleib Gesund und {lk} \
+als würdest du {lk} sein <3")
+
+adj1 = input("Adjektiv: ")                 #
+verb1 = input("Ein Verb: ")                # Wie der ? cmd beim Taschenrechner
+verb2 = input("Noch ein Verb: ")           # "Abfrage User"
+eig = input("Eine Eigenschaft: ")          #
+
+madlib = (f"Programmieren ist so {adj1}! Es erfreut Mich die ganze zeit, \
+weil ich es zu {verb1} finde! <3  Bleib Gesund und {verb2} \
+als würdest du {eig} sein <3")             # Compilation
+
+print(f"{madlib} --- Danke Und Hertzliche Füße von {autor}") # Fertige Ausgabe
+
+# 
+# Copyright: Jack Ruder
+#
