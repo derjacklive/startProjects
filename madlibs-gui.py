efce1344fa81ffca7f41d026a54c09724ad5d389
derjacklive <71508487+derjacklive@users.noreply.github.com>
2023-05-16 17:17:19 +0200

    venv error

--- madlibs-gui.py
+++ madlibs-gui.py
@@ -1,4 +1,9 @@
-#placeholder for the main program
 #
 #Author: Jack Ruder
 #
+
+import tkinter as tk
+import pysimplegui as sg
+
+# Create the window
+sg.window(title="Mad Libs GUI Version 0.1", width=800, height=600, bg="black", ) 
