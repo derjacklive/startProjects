74dc35a5a8a7ec0deeebca937926339769879711
derjacklive <71508487+derjacklive@users.noreply.github.com>
2023-05-17 19:37:06 +0200

    Madlibs Texte angefangen zu füllen

--- madlibs-gui.py
+++ madlibs-gui.py
@@ -4,6 +4,25 @@
 
 import tkinter as tk
 import PySimpleGUI as sg
+import random
 
-# Create the window
-sg.Window(title="Mad Libs GUI Version 0.1", layout=[[]],margins=(100, 50)).read() 
+# Platzhalter Listen + vlgtext 
+
+fplh = "Fülle die Lücken mit den Jeweiligen Adjektiven, Verben und Nomen.  Viel Glück!"
+plhN = "_Ein Nomen_"
+plhV = "_Ein Verb_"
+plhA = "_Ein Adjektiv_"
+plhnrm = ""
+
+# PLaceholder wortarten für übersicht
+
+nomen = ""
+verb = ""
+adjektiv = ""
+
+# Madlib listen
+
+text1 = f"Heute war ein {plhA} Tag, und ich habe mich entschieden, {plhN} zu {plhV}."
+var1 = f"Heute war ein {adjektiv} Tag, und ich habe mich entschieden, {nomen} zu {verb}."
+
+print(text1)
